5d02c09c83c564bbf6fcb48bcc2a83565fd6b32c
neusgo <neusmarianelgo@gmail.com>
2024-08-09 21:56:03 -0400

    Fixes
    Fix pagination and validate user has a card detail

--- src/main/java/com/project/team5/car_bike_rental/controller/RentalController.java
+++ src/main/java/com/project/team5/car_bike_rental/controller/RentalController.java
@@ -8,9 +8,32 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import com.project.team5.car_bike_rental.model.UserProfile;
+import com.project.team5.car_bike_rental.service.UserProfileService;
+
 @RestController
 @RequestMapping("/api/rentals")
 public class RentalController {
+
+    @Autowired
+    private UserProfileService userProfileService;
+
+    @GetMapping("/validateCardNumber")
+    @ResponseBody
+    public boolean validateCardNumber() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        System.out.println("username " + username);
+
+        UserProfile userProfile = userProfileService.getUserProfileByUsername(username);
+        System.out.println("userProfile.getCardNumber()" + userProfile.getCardNumber());
+        System.out.println("userProfile.getCardCvv()" + userProfile.getCardCvv());
+        System.out.println("userProfile.getCardExpiryDate()" + userProfile.getCardExpiryDate());
+        return userProfile != null && userProfile.getCardNumber() != null && !userProfile.getCardNumber().isEmpty() && userProfile.getCardCvv() != null && !userProfile.getCardCvv().isEmpty() && userProfile.getCardExpiryDate() != null;
+    }
+
     @Autowired
     private RentalService rentalService;
 
